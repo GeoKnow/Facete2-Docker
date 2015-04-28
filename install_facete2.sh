af1f15f89c5acba7c3ce044cdc58e1d699bcb9f0
Rene Pietzsch <rpietzsch@brox.de>
2015-04-28 10:36:33 +0200

    cert file workaround

--- install_facete2.sh
+++ install_facete2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# certificate workaround
+mkdir /etc/ssl/private-copy
+mv /etc/ssl/private/* /etc/ssl/private-copy/
+rm -r /etc/ssl/private
+mv /etc/ssl/private-copy /etc/ssl/private
+chmod -R 0700 /etc/ssl/private
+chown -R postgres /etc/ssl/private
+
 service postgresql start
 apt-get -y install facete2-tomcat7
 service postgresql stop
